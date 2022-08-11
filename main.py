f406609090d0502048613760141063a8bf3ccbbc
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-08-11 23:55:08 +0700

    chore: fixing paramiko setup

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from netmiko import ConnectHandler
+import paramiko
+import json, getpass, time
 
-rtr1 = {
-        "device_type": "cisco_ios",
-        "host": "10.10.10.1",
-        "username": "cisco",
-        "password": "cisco"
-}
+with open('inventory.json', 'r') as f:
+    devices = json.load(f)
 
-conn = ConnectHandler(**rtr1)
-output = conn.send_command("show ip interface brief | exclude unassigned")
-print(f"Interface on {rtr1['host']}")
-print(output)
+with open('command.txt', 'r') as f:
+    commands = f.readlines()
+
+username = input('username: ')
+password = getpass.getpass('password: ')
+
+max_buffer = 65535
+
+def clearBuff(connection):
+    if connection.recv_ready():
+        return connection.recv(max_buffer)
+
+for device in devices.keys():
+    outputFileName = device + '_output.txt'
+    connection = paramiko.SSHClient()
+    connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    connection.connect(devices[device]['ip'], username=username, password=password, look_for_keys=False, allow_agent=False)
+    newConnection = connection.invoke_shell()
+    output = clearBuff(newConnection)
+    time.sleep(3)
+    newConnection.send("terminal length 0\n")
+    output = clearBuff(newConnection)
+    with open(outputFileName, 'wb') as f:
+        for command in commands:
+            newConnection.send(command)
+            time.sleep(2)
+            output = newConnection.recv(max_buffer)
+            print(output)
+            f.write(output)
+    newConnection.close()       
